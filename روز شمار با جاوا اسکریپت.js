7a70b1329570710422d9e168f9b54d1f61945dc4
zhack <87863667+codeeditornew@users.noreply.github.com>
2022-03-05 22:39:43 +0330

    Create روز شمار با جاوا اسکریپت.js

---
+++ روز شمار با جاوا اسکریپت.js
@@ -0,0 +1,27 @@
+var a = 3;
+
+switch (a) {
+  case 1:
+    console.log("شنبه");
+    break;
+  case 2:
+    console.log("یک شنبه");
+    break;
+  case 3:
+    console.log("دو شنبه");
+    break;
+  case 4:
+    console.log("سه شنبه");
+    break;
+  case 5:
+    console.log("چهار شنبه");
+    break;
+  case 6:
+    console.log("پنج شنبه");
+    break;
+  case 7:
+    console.log("جمعه");
+    break;
+  default:
+    console.log("نا معتبر");
+  }
